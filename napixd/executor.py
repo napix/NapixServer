06074963bd344072846b954899ce40f1ee0c13de
Grégoire ROCHER <devnull@localhost>
2011-09-07 20:29:38 +0200

    Move run_command into executor

--- napixd/executor.py
+++ napixd/executor.py
@@ -238,3 +238,13 @@ class NullStream(object):
 executor = Executor()
 
 popen = executor.create_job
+
+def run_command(command):
+    """Run a command and return the return code"""
+    return executor.create_job(command,discard_output=True).wait()
+def run_command_or_fail(command):
+    """Run a command and throw an exception if the return code isn't 0"""
+    code = run_command(command)
+    if code != 0:
+        raise subprocess.CalledProcessException,'Oops command <%s> returned %i'%(subprocess.list2cmdline(command),code)
+
