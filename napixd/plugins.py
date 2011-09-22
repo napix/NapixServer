91519ce00542aa45f5ba013c4c57bf9c24884e64
Grégoire ROCHER <gr@enix.fr>
2011-09-22 17:17:32 +0200

    add support for the authentication

--- napixd/plugins.py
+++ napixd/plugins.py
@@ -3,10 +3,19 @@
 
 import functools
 import json
+import logging
 from cStringIO import StringIO
-from bottle import request,HTTPResponse
 
-__all__ = ['ConversationPlugin']
+from urlparse import parse_qs
+from httplib2 import Http
+from urllib import urlencode
+
+import bottle
+from bottle import request,HTTPResponse,HTTPError
+
+from napixd import settings
+
+__all__ = ['ConversationPlugin','AAAPlugin']
 
 class ConversationPlugin(object):
     """Plugin Bottle to convert
@@ -27,3 +36,43 @@ class ConversationPlugin(object):
             return HTTPResponse(buff.getvalue(),
                     header=[('Content-Type', 'application/json')])
         return inner
+
+class AAAPlugin(object):
+    """
+    Authentication, Authorization and Accounting plugins
+    """
+    name = 'authentication_plugin'
+    api = 2
+    logger = logging.getLogger('AAA')
+    def apply(self,callback,route):
+        @functools.wraps(callback)
+        def inner(*args,**kwargs):
+            if bottle.DEBUG and 'authok' in request.GET:
+                return None
+            if not 'HTTP_AUTHORIZATION' in request.META:
+                raise HTTPError(401)
+            msg,l,signature = request.headers['HTTP_AUTHORIZATION'].rpartition(':')
+            if l != ':':
+                self.logger.info('Rejecting request of %s',request.headers['REMOTE_HOST'])
+                raise HTTPError(401,'Need moar authentication')
+            content = parse_qs(msg)
+            for x in content:
+                content[x] = content[x][0]
+            try:
+                if content['host'] != settings.SERVICE:
+                    raise HTTPError(400,'Bad host')
+            except AttributeError:
+                raise HTTPError(400,'No host')
+            content['msg'] = msg
+            content['signature'] = signature
+            self.logger.debug(msg)
+            h=Http()
+            headers = { 'Accept':'application/json',
+                    'Content-type':'application/x-www-form-urlencoded', }
+            body = urlencode(content)
+            resp,content = h.request(settings.AUTH_URL,'POST',body=body,headers=headers)
+            if resp.status != 200:
+                return HTTPError(403,'No auth')
+        return inner
+
+
--- napixd/settings.py
+++ napixd/settings.py
@@ -5,4 +5,6 @@ DEBUG = True
 HOST = '127.0.0.9'
 PORT = 8080
 HANDLERS = ['napixd.fs','napixd.hgrc']
+SERVICE = ''
 
+AUTH_URL = 'http://auth.napix.local/auth/authorization/'
