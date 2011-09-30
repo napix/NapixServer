e43188a17fb0cde3bceac8a667def12224ef3e2c
Grégoire ROCHER <gr@enix.fr>
2011-09-30 16:58:46 +0200

    add comments

--- napixd/resources/by_collection.py
+++ napixd/resources/by_collection.py
@@ -5,36 +5,60 @@ from napixd.exceptions import NotFound
 from . import Collection
 
 class SimpleCollectionResource(dict,Collection):
+    """
+    dictionnaire implementant l'interface des ressource et des collections
+    qui fait par default la laison entre les enfants des SimpleCollection
+    et les sous collections
+    """
     def child(self,subfile):
+        """
+        Recupere la sous collection :param subfile
+        """
         try:
+            #Les sous collections sont installées par la metaclass
+            #dans cette classe
             return getattr(self,subfile)
         except AttributeError:
             raise NotFound,subfile
 
     def get(self):
+        """retourne une copie de self debarassée des properties"""
         return dict(self)
 
     def list(self,filters=None):
+        """Liste les sous-collections disponibles"""
         return self._subresources
 
 class SubResource(object):
+    """Installe une sous ressource dans une SimpleCollection"""
     def __init__(self,subclass):
+        """:param subclass est la sous class de la ressource"""
         self.subclass = subclass
     def __get__(self,instance,owner):
+        """retourne une instance de la sous collection pour la ressource donnée"""
         if instance is None:
             return self.subclass
         return self.subclass(instance)
 
 class SimpleCollection(Collection):
+    """Classe de base de la collection"""
+
+    #class par default
     resource_class = SimpleCollectionResource
 
     def get(self,ident):
+        """filtre le dict de la ressource"""
         child = self.child(ident)
         return dict([(key,child[key])
             for key in child
             if key in self.fields])
 
+    def get_child(self,id_):
+        """Recupère le fils de la collection"""
+        raise NotImplementedError
+
     def child(self,id_):
+        """crée une instance de la resource_class pour la resource"""
         child = self.get_child(id_)
         return self.resource_class(child)
 
--- napixd/resources/by_resource.py
+++ napixd/resources/by_resource.py
@@ -4,9 +4,19 @@
 from . import Collection
 
 class SimpleResource(Collection):
+    """
+    Adapteur qui permert à une classe qui represente une ressource
+    d'implementer l'interface des collections
+    """
     def __init__(self,klass):
+        """
+        crée un nouvel adapteur
+        :param klass est la classe qui represente la resource
+        """
         self.klass = klass
         for x in ('list','create','modify','delete'):
+            #ajoute les propriétés implementées par la classe adaptée
+            #dans cet adapteur
             if hasattr(klass,x):
                 setattr(self,x,getattr(self,'_'+x))
         if not hasattr(klass,'get'):
@@ -14,9 +24,14 @@ class SimpleResource(Collection):
         self.fields = self.klass.fields
 
     def check_id(self,id_):
+        """proxy de la classe adaptée"""
         return self.klass.check_id(id_)
 
     def _get(self,id_):
+        """
+        proxy de la classe adaptée
+        par default renvoie un dictionnaire des champs declarés
+        """
         child=self.klass.child(id_)
         res = {}
         for x in self.fields:
@@ -24,14 +39,18 @@ class SimpleResource(Collection):
         return res
 
     def _list(self,filters=None):
+        """proxy de la classe adaptée"""
         return self.klass.list(filers = None)
 
     def _create(self,data):
+        """proxy de la classe adaptée"""
         return self.klass.create(data)
 
     def _modify(self,id_,data):
-        return self.child(id_).modify(data)
+        """proxy de la classe adaptée"""
+        return self.klass.child(id_).modify(data)
 
     def _delete(self,id_):
-        return self.child(id_).delete()
+        """proxy de la classe adaptée"""
+        return self.klass.child(id_).delete()
 
