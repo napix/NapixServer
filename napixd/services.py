eb186feb6c41f0670ff461a19b942cc2f07a40a9
Grégoire ROCHER <gr@enix.fr>
2011-09-30 17:12:56 +0200

    Add comment to services.py

--- napixd/services.py
+++ napixd/services.py
@@ -9,6 +9,10 @@ from bottle import HTTPError
 from napixd.exceptions import NotFound,ValidationError,Duplicate
 
 class ArgumentsPlugin(object):
+    """
+    Plugin qui filtre les mots clés de la requête pour les passer par liste
+    Workaround du fait que bottle ne passe les arguments que par mot clé
+    """
     name='argument'
     api = 2
     def apply(self,callback,route):
@@ -27,14 +31,18 @@ class ArgumentsPlugin(object):
                         itertools.count())))
 
 class Service(object):
+    """Class qui sert d'interface entre une application bottle et les collections"""
     def __init__(self,collection):
+        """crée un nouveau service pour la collection donnée"""
         self.collection = collection()
         self.url = collection.__name__.lower()
 
     def setup_bottle(self,app):
+        """Enregistre le service sur l'application bottle passée"""
         self._setup_bottle(app,self.collection,'/'+self.url,0)
 
     def _setup_bottle(self,app,collection,prefix,level):
+        """Fonction récursive pour enregister une collection et ses sous-collections"""
         app.route('%s'%(prefix),method='ANY',callback=self.as_resource,apply=ArgumentsPlugin)
         next_prefix = '%s/:f%i'%(prefix,level)
         app.route('%s/'%(prefix),method='ANY',callback=self.as_collection,apply=ArgumentsPlugin)
@@ -46,21 +54,29 @@ class Service(object):
                             '%s/:f%i'%(next_prefix,level+1),level+2)
 
     def as_resource(self,request,path):
+        """Callback appellé pour une requete demandant une collection"""
         return ServiceResourceRequest(request,path,
                 self.collection).handle()
 
     def as_collection(self,request,path):
+        """Callback appellé pour une requete demandant une resource"""
         return ServiceCollectionRequest(request,path,
                 self.collection).handle()
 
 class ServiceRequest(object):
+    """Objet créé pour servir une requete"""
     def __init__(self,request,path,collection):
+        """
+        Crée l'objet qui sert la requete request sur collection
+        en demandant l'objet designé par path
+        """
         self.request = request
         self.method = request.method
         self.base_collection = collection
         self.path = path
 
     def check_datas(self,collection):
+        """Verifie que les champs passés sont dans le les champs de la collection"""
         data = {}
         for x in self.request.data:
             if x in collection.fields:
@@ -68,13 +84,21 @@ class ServiceRequest(object):
         return data
 
     def get_collection(self):
+        """
+        Récupere la collection correspondante à la requete
+        """
         node = self.base_collection
         for child in self.path:
+            #verifie l'id de la collection
             child_id = node.check_id(child)
             node = node.child(child_id)
         return node
 
     def get_callback(self,collection):
+        """
+        recupere la callback de collection
+        Si elle n'est pas disponible renvoie une erreur 405 avec les methodes possibles
+        """
         try:
             return getattr(collection,self.METHOD_MAP[self.method])
         except (AttributeError,KeyError):
@@ -86,10 +110,17 @@ class ServiceRequest(object):
                     header=[ ('allow',','.join(available_methods))])
 
     def handle(self):
+        """
+        gère l'appel
+        """
         try:
+            #obtient l'object designé
             collection = self.get_collection()
+            #recupère les données valides pour cet objet
             datas =  self.check_datas(collection)
+            #recupère la vue qui va effectuer la requete
             callback = self.get_callback(collection)
+            #recupere les arguments a passer a cette vue
             args = self.get_args(datas)
             return callback(*args)
         except ValidationError,e:
@@ -103,6 +134,7 @@ class ServiceRequest(object):
 
 
 class ServiceCollectionRequest(ServiceRequest):
+    #association de verbes HTTP aux methodes python
     METHOD_MAP = {
         'POST':'create',
         'GET':'list'
@@ -123,6 +155,7 @@ class ServiceResourceRequest(ServiceRequest):
         }
     def __init__(self,request,path,collection):
         super(ServiceResourceRequest,self).__init__(request,path[:-1],collection)
+        #extrait le dernier element du path qui sera la ressource
         self.resource_id = path[-1]
 
     def get_args(self,datas):
@@ -131,6 +164,7 @@ class ServiceResourceRequest(ServiceRequest):
         return (self.resource_id,)
     def get_collection(self):
         collection = super(ServiceResourceRequest,self).get_collection()
+        #verifie l'identifiant de la resource aussi
         self.resource_id = collection.check_id(self.resource_id)
         return collection
 
