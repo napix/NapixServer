9513d2a93425f44804d1d56a4fd70b92d9d5c823
Grégoire ROCHER <gr@enix.fr>
2011-09-30 17:28:54 +0200

    add coments

--- napixd/resources/__init__.py
+++ napixd/resources/__init__.py
@@ -4,6 +4,7 @@
 from napixd.exceptions import ValidationError
 
 class Collection(object):
+    """classe de base d'une collection"""
     def check_id(self,id_):
         if id_ ==  '':
             raise ValidationError,'ID cannot be empty'
