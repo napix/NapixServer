3deea8469cbf6225a2fb55897d722be9ffd1c368
Grégoire ROCHER <gr@enix.fr>
2011-10-07 18:09:46 +0200

    add doc about the public interface

--- napixd/threadator.py
+++ napixd/threadator.py
@@ -12,6 +12,106 @@ logger = logging.getLogger('threadator')
 
 __all__ = ['threadator','thread_manager','background_task']
 
+"""
+Un object threadator et un object bacground_task relié sont créés et doivent être utilisés
+
+Threadator definie un objet qui permet de lancer simplement des fonctions de manière asynchrone
+Il est possible de définir des callbacks qui seront appelés suivant l'evolution de l'execution du thread.
+La fonction executé dispose d'une reference sur le thread et peux mettre a jour un statut.
+
+Interface threadator
+    do_async(fn:callable,<params>) -> thread
+    il prends un argument obligatoire
+        fn(thread,*args,**kwargs)
+            callable qui sera executé de manière asynchrone
+            cette fonction prends comme premier paramètre le ThreadWrapper dans lequel le thread s'executera
+            et lui permet de mettre a jour son status
+    do_async peut prendre en parametre optionnels
+    callbacks:
+        on_success(result)
+            appelé si la fonction fini sans avoir produit d'exception.
+            result est le resultat de l'execution du script
+        on_failure(exception)
+            appelé si la fonction jete une exception, prends en parametre
+            l'exception jeté.
+        on_end()
+            appelé quand la fonction a fini quelque soit le resultat.
+    lancement:
+        args
+            tuple d'arguments passé a l'execution de la fonction
+        kwargs
+            dictionnaire de mot clés passé à la fonction
+
+Le threadator agit aussi comme un dictionnaire de tout les threads qui'il execute actuellement
+il definit
+    [ thread_id ] -> thread
+        retourne le thread ayant l'identifiant thread_id
+    keys -> thread[]
+        retourne tout les identifiant de thread
+
+Enfin le threadator s'intègre avec les autres composant du serveur web, il definit les methodes start et stop
+    start()
+        demarre le threadator
+    stop()
+        arrete le threadator dans la seconde qui suit. Les thread encore en cours continuent.
+
+Le resultat de l'appel a do_async est un ThreadWrapper qui encapsule le thread pour lui donner accès
+a des fonctionalités de controle.
+Interface ThreadWrapper
+    status
+        sert de transport pour une valeur arbitraire fourni par la fonction asynchrone
+        pour les autres threads.
+        Exemple:
+        def mail_everyone(thread,users):
+            total = len(users)
+            for idx,user in enumerate(users):
+                thread.status = idx/total*100.
+                mail(user)
+        wrapper = threadator.do_aync(mail,args=(User.objects.all(),)) # retourne immediatement
+        # wrapper.status va prendre le pourcentage de mails envoyés
+    execution_state
+        sert de transport pour l'état dans lequel le thread se trouve actulement
+        Il peut prendre les valeurs suivant
+            CREATED
+                valeur initiale du thread
+            RUNNING
+                Un thread a été créé et la fonction lancée dedans
+            RETURNED
+                la fonction a retourné une valeur
+                on_success va être appelé
+            EXCEPTION
+                la fontion a jeté une exception
+                on_failure  va être appelé
+            FINISHING
+                la fonction a fini
+                on_end va être appelé
+            CLOSED
+                le thread a fini
+
+Les objects bacgroundtasker servent de décorateur pour une fonction arbitraire
+les appels a cette fonction seront fait dans un autre thread
+
+exemple
+
+@bacground_task
+def send_mail(users):
+    for u in users:
+        send_mail(user)
+
+def view(request):
+    send_mail(User.objects.all()) #retourne immediatement
+
+Le decorator bacground_task peux prendre les arguments mot-clés suivant:
+    drop_thread:
+        par default le threadator envoie l'instance du thread wrapper comme premier
+        argument de la fonction executé.
+        ce paramètre permet de supprimer ce thread pour s'interfacer de manière plus transparente.
+        Par default il est a True.
+    args,kwargs,on_success,on_failure,on_end:
+        voir do_async
+
+"""
+
 class BackgroundTasker(object):
     """
     Instance of BackgroundTasker can be used to decorate
