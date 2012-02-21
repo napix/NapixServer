6fcc5962eb8eb393bd92f8c129ed0d6f683a31f5
Grégoire ROCHER <gr@enix.fr>
2012-02-21 09:09:30 +0100

    add store

---
+++ napixd/store.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from UserDict import IterableUserDict
+import os
+import cPickle as pickle
+import redis
+from .conf import Conf
+
+class FileStore( IterableUserDict):
+    def __init__( self, collection, path = None ):
+        path = path != None and path or self.get_path() 
+        self.file_path = os.path.join( path , collection)
+        if not os.path.isdir( path):
+            os.makedirs( path, 0700)
+
+        try:
+            self.data = pickle.load( open(self.file_path, 'r'))
+        except IOError:
+            self.data = {}
+
+    def save(self):
+        pickle.dump( self.data, open( self.file_path, 'w'))
+
+    def get_path(self):
+        return Conf.get_default().get( 'Napix.Store' ).get( 'path') or '/var/lib/napix'
+
+
+class RedisStore( IterableUserDict):
+    def get_default_options( self):
+        return Conf.get_default().get( 'Napix.Store').get('redis') or {}
+    def __init__( self, collection, options= None):
+        options = options != None and options or self.get_default_options()
+        self.redis = redis.Redis( **options )
+        self.collection = collection
+        data = self.redis.get( collection)
+        self.data = pickle.loads( data ) if data else {}
+
+    def save( self):
+        self.redis.set( self.collection, pickle.dumps( self.data ))
+
---
+++ napixd/tests/test_store.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import unittest2
+import os
+import shutil
+from redis import Redis
+from napixd.store import FileStore, RedisStore
+
+class BaseTestStore(object):
+    def setUp( self):
+        store = self.store_class( '_napix_store_test')
+        store['value'] = 1
+        store.save()
+    def testEmpty(self):
+        collection = self.store_class( '_napix_store_test1')
+        self.assertEqual( len( collection), 0)
+
+    def testRestore(self):
+        store = self.store_class( '_napix_store_test')
+        self.assertEqual(store['value'], 1)
+
+    def testNotSave(self):
+        store = self.store_class( '_napix_store_test')
+        store['value'] = 2
+        store = self.store_class( '_napix_store_test')
+        self.assertEqual(store['value'], 1)
+
+class TestFileStore( BaseTestStore, unittest2.TestCase):
+    store_class = FileStore
+    @classmethod
+    def setUpClass( cls):
+        if os.path.isfile( '/tmp/_napix_store_test1'):
+            os.unlink( '/tmp/_napix_store_test1')
+        if os.path.isdir( '/tmp/_napix_store/test_dir'):
+            shutil.rmtree( '/tmp/_napix_store/test_dir')
+    def testCreateDirectory(self):
+        collection = self.store_class( '_napix_store_test', '/tmp/_napix_store/test_dir')
+        self.assertEqual( len( collection), 0)
+        collection.save()
+
+class TestRedisStore( BaseTestStore, unittest2.TestCase):
+    store_class = RedisStore
+    @classmethod
+    def setUpClass( cls):
+        redis = Redis()
+        redis.delete( '_napix_store_test1', None)
+
+if __name__ == '__main__':
+    unittest2.main()
