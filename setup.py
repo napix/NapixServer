c248b66cba8b7257f636702a3c8d5b83b27f0669
Jérôme Petazzoni <jp@enix.org>
2012-03-21 12:42:34 -0700

    switch from distutils to setuptools; include dependancies in setup.py; add helper to include data files

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from distutils.core import setup
+import os
+from setuptools import setup, find_packages
+
+def find_data(root_dir):
+	return sum([
+		[os.path.join(dirname, filename)
+		 for filename in filelist]
+		for dirname, dirlist, filelist
+		in os.walk(root_dir)], [])
 
 setup(name="napixd",
         version="0.1",
-        packages=['napixd']
+        packages=find_packages(),
+	install_requires=[
+		'bottle==0.10.8',
+		'httplib2==0.7.4',
+		'Rocket==1.2.4',
+		],
+	data_files=[
+		('napixd/conf', ['napixd/conf/settings.json']),
+		('napixd/web', find_data('napixd/web')),
+		],
         )
