53bbdd45249679a7193524e758f4c8b13f4bbdd6
Grégoire ROCHER <gr@enix.fr>
2012-05-23 17:30:21 +0200

    changed the script place

--- setup.py
+++ setup.py
@@ -5,22 +5,25 @@ import os
 from setuptools import setup, find_packages
 
 def find_data(root_dir):
-	return sum([
-		[os.path.join(dirname, filename)
-		 for filename in filelist]
-		for dirname, dirlist, filelist
-		in os.walk(root_dir)], [])
+    return sum([
+        [os.path.join(dirname, filename)
+            for filename in filelist]
+        for dirname, dirlist, filelist
+        in os.walk(root_dir)], [])
 
 setup(name="napixd",
         version="0.1",
         packages=find_packages(),
-	install_requires=[
-		'bottle==0.10.8',
-		'httplib2==0.7.4',
-		'Rocket==1.2.4',
-		],
-	data_files=[
-		('napixd/conf', ['napixd/conf/settings.json']),
-		('napixd/web', find_data('napixd/web')),
-		],
+    install_requires=[
+        'bottle==0.10.8',
+        'httplib2==0.7.4',
+        'Rocket==1.2.4',
+        ],
+    scripts=[
+        'bin/napixd'
+        ],
+    data_files=[
+        ('napixd/conf', ['napixd/conf/settings.json']),
+        ('napixd/web', find_data('napixd/web')),
+        ],
         )
