7a6ece3e429c52df5d5a44c62397f4d57c5e4fca
Grégoire ROCHER <gr@enix.fr>
2012-06-20 07:29:00 +0200

    Add implementations of redis store

--- napixd/store.py
+++ napixd/store.py
@@ -1,43 +1,166 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from UserDict import IterableUserDict
 import os
+import collections
 import cPickle as pickle
 from .conf import Conf
 
-class FileStore( IterableUserDict):
+class BaseStore( collections.MutableMapping):
+    def __init__( self, data):
+        self.data = data or {}
+    def __getitem__( self, key):
+        return self.data[key]
+    def __setitem__( self, key, value):
+        self.data[key] = value
+    def __delitem__( self, key, value):
+        self.data[key] = value
+    def __iter__(self):
+        return iter(self.keys())
+    def __len__( self):
+        return len( self.keys())
+    def keys(self):
+        return self.data.keys()
+    def incr(self, key, incr=1):
+        self[key] += 1
+        return self[key]
+    def drop(self):
+        pass
+    def save(self):
+        pass
+
+class FileStore( BaseStore ):
+    PATH =  '/var/lib/napix'
     def __init__( self, collection, path = None ):
-        path = path != None and path or self.get_path() 
+        path = path != None and path or self.get_path()
         self.file_path = os.path.join( path , collection)
         if not os.path.isdir( path):
             os.makedirs( path, 0700)
-
         try:
-            self.data = pickle.load( open(self.file_path, 'r'))
+            data = pickle.load( open(self.file_path, 'r'))
         except IOError:
-            self.data = {}
+            data = {}
+        super( FileStore, self).__init__( data )
+
+    def drop( self):
+        if os.path.isfile(self.file_path):
+            os.unlink(self.file_path)
 
     def save(self):
         pickle.dump( self.data, open( self.file_path, 'w'))
 
     def get_path(self):
-        return Conf.get_default().get( 'Napix.Store' ).get( 'path') or '/var/lib/napix'
-
-
+        return Conf.get_default().get( 'Napix.Store' ).get( 'path') or self.PATH
 try:
     import redis
-    class RedisStore( IterableUserDict):
+    class BaseRedisStore( BaseStore ):
         def get_default_options( self):
             return Conf.get_default().get( 'Napix.Store').get('redis') or {}
         def __init__( self, collection, options= None):
             options = options != None and options or self.get_default_options()
             self.redis = redis.Redis( **options )
             self.collection = collection
-            data = self.redis.get( collection)
-            self.data = pickle.loads( data ) if data else {}
+
+        def drop(self):
+            self.redis.delete(self.collection)
+
+    class RedisStore( BaseRedisStore ):
+        def __init__(self, collection, options=None):
+            super(RedisStore, self).__init__(collection, options)
+            try:
+                data = self.redis.get( collection)
+                self.data = pickle.loads( data ) if data else {}
+            except redis.ResponseError:
+                pass
 
         def save( self):
             self.redis.set( self.collection, pickle.dumps( self.data ))
+
+    class RedisHashStore(BaseRedisStore):
+        def __contains__( self, key):
+            return self.redis.hexists( self.collection, key)
+
+        def __delitem__(self, item):
+            self.redis.hdel( self.collection, item)
+
+        def __getitem__(self, item):
+            value = self.redis.hget( self.collection, item)
+            if value is None:
+                raise KeyError, item
+            return value
+
+        def __len__( self):
+            return self.redis.hlen( self.collection)
+
+        def __setitem__(self, item, value):
+            return self.redis.hset( self.collection, item, value)
+
+        def keys(self):
+            return self.redis.hkeys(self.collection)
+
+        def values( self):
+            return self.redis.hval( self.collection)
+
+        def update( self, other_dict):
+            self.redis.hmset(self.collection, other_dict)
+
+        def items( self):
+            return self.redis.hgetall(self.collection).items()
+
+        def __eq__(self, other):
+            if isinstance( other, RedisHashStore):
+                return self.collection == other.collection
+            else:
+                return super( RedisHashStore, self).__eq__( other)
+
+        def incr(self, key, incr=1):
+            return self.redis.hincrby( self.collection, key, incr)
+
+    class RedisKeyStore(BaseRedisStore):
+        def _make_key(self, key):
+            return '{0}:{1}'.format( self.collection, key)
+        def _all_keys( self):
+            return '{0}:{1}'.format( self.collection, '*')
+
+        def __contains__( self, key):
+            return self.redis.exists( self._make_key( key))
+
+        def __delitem__(self, item):
+            self.redis.delete( self._make_key( item))
+
+        def __getitem__(self, item):
+            value = self.redis.get( self._make_key( item))
+            if value is None:
+                raise KeyError, item
+            return value
+
+        def __iter__(self):
+            return iter(self.keys())
+
+        def __len__( self):
+            return len( self.keys())
+
+        def __setitem__(self, item, value):
+            return self.redis.set( self._make_key( item), value)
+
+        def keys(self):
+            return [ x.partition(':')[2] for x in self.redis.keys( self._all_keys()) ]
+
+        def update( self, other_dict):
+            self.redis.mset(self.collection, other_dict)
+
+        def __eq__(self, other):
+            if isinstance( other, RedisHashStore):
+                return self.collection == other.collection
+            else:
+                return super( RedisHashStore, self).__eq__( other)
+
+        def incr(self, key, incr=1):
+            return self.redis.incr( self._make_key( key), incr)
+
+        def drop(self):
+            for x in self.keys():
+                del self[x]
+
 except ImportError:
     pass
--- tests/test_store.py
+++ tests/test_store.py
@@ -7,7 +7,7 @@ import os
 import shutil
 try:
     from redis import Redis
-    from napixd.store import RedisStore
+    from napixd.store import RedisStore, RedisHashStore, RedisKeyStore
 except ImportError:
     Redis = False
 
@@ -16,42 +16,75 @@ from napixd.store import FileStore
 class BaseTestStore(object):
     def setUp( self):
         store = self.store_class( '_napix_store_test')
-        store['value'] = 1
+        store['int'] = 1
+        store['mpm'] = 'prefork'
         store.save()
+
+    @classmethod
+    def setUpClass( cls):
+        cls.store_class('_napix_store_test').drop()
+        cls.store_class('_napix_store_test1').drop()
+
+    def testDrop( self):
+        collection = self.store_class( '_napix_store_test')
+        collection.drop()
+        collection = self.store_class( '_napix_store_test')
+        self.assertEqual( len( collection), 0)
+
     def testEmpty(self):
         collection = self.store_class( '_napix_store_test1')
         self.assertEqual( len( collection), 0)
 
-    def testRestore(self):
+    def testRestoreInt(self):
         store = self.store_class( '_napix_store_test')
-        self.assertEqual(store['value'], 1)
+        self.assertEqual(int( store['int']), 1)
+
+    def testRestoreString(self):
+        store = self.store_class( '_napix_store_test')
+        self.assertEqual(store['mpm'], 'prefork')
 
     def testNotSave(self):
         store = self.store_class( '_napix_store_test')
-        store['value'] = 2
+        store['int'] = 2
         store = self.store_class( '_napix_store_test')
-        self.assertEqual(store['value'], 1)
+        self.assertEqual(store['int'], 1)
+
+    def testIncrement( self):
+        store = self.store_class( '_napix_store_test')
+        self.assertEqual(store.incr('int'), 2)
+        store.save()
+
+        store = self.store_class( '_napix_store_test')
+        self.assertEqual(int(store['int']), 2)
+
+    def testWrongKey( self):
+        collection = self.store_class( '_napix_store_test')
+        self.assertRaises( KeyError, lambda :collection['nokey'])
+
+    def testKey(self):
+        collection = self.store_class( '_napix_store_test')
+        self.assertEqual( set(collection.keys()), set( ('int', 'mpm' ) ))
 
 class TestFileStore( BaseTestStore, unittest2.TestCase):
     store_class = FileStore
-    @classmethod
-    def setUpClass( cls):
-        if os.path.isfile( '/tmp/_napix_store_test1'):
-            os.unlink( '/tmp/_napix_store_test1')
-        if os.path.isdir( '/tmp/_napix_store/test_dir'):
-            shutil.rmtree( '/tmp/_napix_store/test_dir')
     def testCreateDirectory(self):
         collection = self.store_class( '_napix_store_test', '/tmp/_napix_store/test_dir')
         self.assertEqual( len( collection), 0)
         collection.save()
 
-unittest2.skipIf( not Redis, 'There is not Redis libs available')
-class TestRedisStore( BaseTestStore, unittest2.TestCase):
+@unittest2.skipIf( not Redis, 'There is not Redis libs available')
+class TestRedisStore(BaseTestStore, unittest2.TestCase):
     store_class = RedisStore
-    @classmethod
-    def setUpClass( cls):
-        redis = Redis()
-        redis.delete( '_napix_store_test1', None)
+
+@unittest2.skipIf( not Redis, 'There is not Redis libs available')
+class TestRedisHashStore(BaseTestStore, unittest2.TestCase):
+    store_class = RedisHashStore
+    testNotSave = unittest2.expectedFailure( BaseTestStore.testNotSave)
+
+@unittest2.skipIf( not Redis, 'There is not Redis libs available')
+class TestRedisKeyStore(BaseTestStore, unittest2.TestCase):
+    store_class = RedisKeyStore
+    testNotSave = unittest2.expectedFailure( BaseTestStore.testNotSave)
 
 if __name__ == '__main__':
     unittest2.main()
