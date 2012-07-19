10bd4b173ad2b94546bc6e526d728745fb1e71fa
Grégoire ROCHER <gr@enix.fr>
2012-07-19 18:12:43 +0200

    Add invoice service

---
+++ napixd/contrib/invoice/service.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from napixd.connectors.django import DjangoModelManager, DjangoRelatedModelManager, DjangoImport
+
+__all__ = ( 'CompanyManager', )
+
+with DjangoImport():
+    from napixd.contrib.invoice.models import Invoice, Company, Product
+
+
+class CompanyManager( DjangoModelManager):
+    name = 'company'
+    model = Company
+    managed_class = [ 'InvoiceManager' ]
+
+
+class InvoiceManager(DjangoRelatedModelManager):
+    name = 'invoices'
+    model = Invoice
+    related_to = 'Company'
+    model_fields_exclude = ( 'serial_number', )
+    managed_class = [ 'ProductManager' ]
+
+
+class ProductManager(DjangoRelatedModelManager):
+    name = 'products'
+    model = Product
+    related_to = 'Invoice'
