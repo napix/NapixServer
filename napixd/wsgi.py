141eb2efa0942459c1b6c2e98e0cde5568150b51
Grégoire ROCHER <gr@enix.fr>
2013-01-11 03:12:22 +0100

    Set the napixd HOME when importing wsgi

--- napixd/wsgi.py
+++ napixd/wsgi.py
@@ -2,7 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import os
+import napixd
 from napixd.launcher import Setup
+
+path = os.path.realpath( os.path.join( os.path.dirname(__file__), '..' ))
+sys.path.append(path)
+napixd.HOME = path
 
 options = set( sys.argv)
 options.add( 'quiet')
