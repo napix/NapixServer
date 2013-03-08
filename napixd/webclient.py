db4a741b330fa38ff3c07afbf5b06075a396f28e
Grégoire ROCHER <gr@enix.fr>
2013-03-08 13:03:13 +0100

    force content type of index.html

--- napixd/webclient.py
+++ napixd/webclient.py
@@ -7,11 +7,15 @@ class WebClient( bottle.Bottle):
     def __init__(self, root):
         super( WebClient, self).__init__( autojson=False)
         self.root = root
-        self.get('/', callback=self.static)
+        self.get('/', callback=self.index)
         self.get('/<filename:path>', callback=self.static)
 
     def setup_bottle(self, app):
         app.mount( '/_napix_js', self)
 
-    def static(self, filename = 'index.html' ):
+    def index(self):
+        return bottle.static_file( 'index.html', root=self.root,
+                content_type = 'text/html; charset=UTF-8')
+
+    def static(self, filename):
         return bottle.static_file( filename, root=self.root )
