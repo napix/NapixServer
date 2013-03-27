cee9b37f8c0618afdb651be8cd7e80bbeff708d1
Grégoire ROCHER <gr@enix.fr>
2013-03-27 20:21:51 +0100

    Test the services plugin

---
+++ tests/test_services/test_plugin.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+import unittest
+import mock
+
+from napixd.services.plugins import ArgumentsPlugin
+
+class TestPlugin(unittest.TestCase):
+    def setUp(self):
+        self.arguments = ArgumentsPlugin()
+        def fn(args):
+            self.args = args
+        self.cb = self.arguments.apply( fn, mock.Mock())
+
+    def test_call_kw(self):
+        self.cb(f0 = 'a', f1='b', f2='c')
+        self.assertEqual( tuple(self.args), ( 'a', 'b', 'c'))
+
+    def test_call_kw_gap(self):
+        self.cb(f0 = 'a', f1='b', f3='c')
+        self.assertEqual( tuple(self.args), ( 'a', 'b'))
+
