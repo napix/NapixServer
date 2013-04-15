4ef0e954c745b7fe5d83eb3ef8817be2f1f214d1
Grégoire ROCHER <gr@enix.fr>
2013-04-15 19:19:03 +0200

    Set the mock class so it passes subclass check

--- tests/mock/store_backend.py
+++ tests/mock/store_backend.py
@@ -3,4 +3,10 @@
 
 import mock
 
-MockBackend = mock.Mock()
+from napixd.store.backends import BaseBackend
+
+class MockBackend(BaseBackend):
+    return_value = mock.Mock()
+
+    def __new__(self, options):
+        return self.return_value
