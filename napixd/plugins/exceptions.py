b7f03a1503a56705b8f402b6c9a59e8eae806777
Grégoire ROCHER <gr@enix.fr>
2013-04-27 22:48:15 +0200

    Fix the traceback retruned by the exception catcher

--- napixd/plugins/exceptions.py
+++ napixd/plugins/exceptions.py
@@ -10,12 +10,17 @@ import logging
 
 import bottle
 
+import napixd
+
 class ExceptionsCatcher(object):
     name = 'exceptions_catcher'
     api = 2
     logger = logging.getLogger('Napix.Errors')
+
     def __init__(self, show_errors=False):
         self.show_errors = show_errors
+        self.napix_path = os.path.dirname( napixd.__file__)
+
     def apply(self,callback,route):
         """
         This plugin run the view and catch the exception that are not HTTPResponse.
@@ -35,11 +40,10 @@ class ExceptionsCatcher(object):
                 filename, lineno, function_name, text = traceback.extract_tb(last_traceback)[-1]
                 if self.show_errors:
                     traceback.print_exc()
-                napix_path = os.path.realpath( os.path.join( os.path.dirname( __file__)))
                 all_tb = [ dict( zip( ('filename', 'line', 'in', 'call'), x))
                         for x in traceback.extract_tb( last_traceback ) ]
                 extern_tb = [ x for x in all_tb
-                        if x['filename'].startswith(napix_path) ]
+                        if not x['filename'].startswith(self.napix_path) ]
                 self.logger.error('%s on %s failed with %s (%s)',  method, path,
                         e.__class__.__name__, str(e) )
                 res = {
--- tests/test_plugins/test_plugins.py
+++ tests/test_plugins/test_plugins.py
@@ -14,7 +14,7 @@ class TestExceptionCatcher( unittest2.TestCase):
         self.requester = mock.patch( 'bottle.request', method='GET', path='/')
 
     def setUp(self):
-        exc = ExceptionsCatcher()
+        self.exc = exc = ExceptionsCatcher()
         self.requester.start()
         self.cb = exc.apply( self.success, mock.Mock())
 
@@ -38,4 +38,46 @@ class TestExceptionCatcher( unittest2.TestCase):
         resp = self.cb( previous_error)
         self.assertIs( resp, previous_error)
 
+    def test_filter(self):
+        self.exc.napix_path = '/a/b'
+        with mock.patch( 'napixd.plugins.exceptions.traceback') as traceback:
+            traceback.extract_tb.return_value = [
+                    [ '/a/b/fname', 12, 'function', 'patoum(1, 2, 3)' ],
+                    [ '/c/d/fname', 12, 'function', 'patoum(1, 2, 3)' ],
+                    ]
+            resp = self.cb( Exception())
 
+        body = json.loads( resp.body)
+        self.assertEqual( body['traceback'], [
+            {
+                'filename': '/c/d/fname',
+                'line': 12,
+                'in': 'function',
+                'call': 'patoum(1, 2, 3)',
+                }
+            ])
+
+    def test_filter_intern(self):
+        self.exc.napix_path = '/a/b'
+        with mock.patch( 'napixd.plugins.exceptions.traceback') as traceback:
+            traceback.extract_tb.return_value = [
+                    [ '/a/b/fname', 12, 'function', 'patoum(1, 2, 3)' ],
+                    [ '/a/b/fname', 12, 'function', 'patoum(1, 2, 3)' ],
+                    ]
+            resp = self.cb( Exception())
+
+        body = json.loads( resp.body)
+        self.assertEqual( body['traceback'], [
+            {
+                'filename': '/a/b/fname',
+                'line': 12,
+                'in': 'function',
+                'call': 'patoum(1, 2, 3)',
+                },
+            {
+                'filename': '/a/b/fname',
+                'line': 12,
+                'in': 'function',
+                'call': 'patoum(1, 2, 3)',
+                }
+            ])
