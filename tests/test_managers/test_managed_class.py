a83e083d0565030b4b9274b3506f2e63169fc8da
Grégoire ROCHER <gr@enix.fr>
2013-06-06 18:29:48 +0200

    Add managers test

---
+++ tests/test_managers/test_managed_class.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+import unittest
+import mock
+
+from napixd.managers.base import Manager, ManagerType
+from napixd.managers.managed_classes import ManagedClass
+
+
+class TestManagedClass( unittest.TestCase):
+    def setUp(self):
+        self.manager = mock.MagicMock( spec=Manager, __name__='my-mock')
+
+    def test_is_resolved(self):
+        mc = ManagedClass( 'a.b.C')
+        self.assertFalse( mc.is_resolved())
+
+    def test_name(self):
+        mc = ManagedClass( self.manager)
+        self.assertEqual( mc.get_name(), self.manager.get_name())
+
+    def test_rewrite_name(self):
+        mc = ManagedClass( self.manager, name='other')
+        self.assertEqual( mc.get_name(), 'other')
+
+    def test_rewrite_extractor(self):
+        extractor = mock.Mock()
+        mc = ManagedClass( self.manager, extractor=extractor)
+        self.assertEqual( mc.extractor, extractor)
+
+class TestBuilder( unittest.TestCase):
+    class SubManager( Manager):
+        pass
+
+    def testNone( self):
+        m = ManagerType( 'Manager', (Manager,), {
+            'managed_class': None
+            })
+        self.assertEqual( m.direct_plug(), None)
+        self.assertEqual( m.get_managed_classes(), [])
+
+    def testFalse( self):
+        m = ManagerType( 'Manager', (Manager,), {
+            'managed_class': [ self.SubManager ]
+            })
+        self.assertEqual( m.direct_plug(), False)
+        self.assertEqual( m.get_managed_classes(), [ ManagedClass( self.SubManager) ])
+
+    def testFalseManagedClass( self):
+        m = ManagerType( 'Manager', (Manager,), {
+            'managed_class': [ ManagedClass( self.SubManager, 'ploc') ]
+            })
+        self.assertEqual( m.direct_plug(), False)
+        self.assertEqual( m.get_managed_classes(), [ ManagedClass( self.SubManager, 'ploc' ) ])
+
+    def testFalseString( self):
+        m = ManagerType( 'Manager', (Manager,), {
+            'managed_class': [ 'abc' ]
+            })
+        self.assertEqual( m.direct_plug(), False)
+        self.assertEqual( m.get_managed_classes(), [ ManagedClass( 'abc' ) ])
+
+    def testTrue( self):
+        m = ManagerType( 'Manager', (Manager,), {
+            'managed_class': self.SubManager
+            })
+        self.assertEqual( m.direct_plug(), True)
+
+    def testTrueString( self):
+        m = ManagerType( 'Manager', (Manager,), {
+            'managed_class': 'abc'
+            })
+        self.assertEqual( m.direct_plug(), True)
+        self.assertEqual( m.get_managed_classes(), [
+            ManagedClass( 'abc')
+            ])
+
+    def testTrueManagedClass(self):
+        m = ManagerType( 'Manager', (Manager,), {
+            'managed_class': ManagedClass( 'abc', 'ploc')
+            })
+        self.assertEqual( m.direct_plug(), True)
+        self.assertEqual( m.get_managed_classes(), [
+            ManagedClass( 'abc', 'ploc')
+            ])
