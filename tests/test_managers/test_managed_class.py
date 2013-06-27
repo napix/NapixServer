1dce1bd822329c2b514131f527e15609cc8f4ce3
Grégoire ROCHER <gr@enix.fr>
2013-06-27 13:10:56 +0200

    Test managed classes resolving mechanism

--- tests/test_managers/test_managed_class.py
+++ tests/test_managers/test_managed_class.py
@@ -18,6 +18,15 @@ class TestManagedClass( unittest.TestCase):
         mc = ManagedClass( 'a.b.C')
         self.assertFalse( mc.is_resolved())
 
+    def test_resovle(self):
+        mc = ManagedClass( 'a.b.C')
+        mc.resolve( self.manager)
+        self.assertTrue( mc.is_resolved())
+
+    def test_name_not_resolved(self):
+        mc = ManagedClass( 'a.b.C')
+        self.assertRaises( ValueError, mc.get_name)
+
     def test_name(self):
         mc = ManagedClass( self.manager)
         self.assertEqual( mc.get_name(), self.manager.get_name())
