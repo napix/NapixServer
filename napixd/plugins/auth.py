8afdefb815173e971ace40c81f365021cb95fa61
Grégoire ROCHER <gr@enix.org>
2013-08-01 19:43:21 +0200

    Add logger for the paths

--- napixd/plugins/auth.py
+++ napixd/plugins/auth.py
@@ -153,10 +153,10 @@ class BaseAAAPlugin(object):
                 path += '?' + bottle.request.query_string
             if content['method'] != bottle.request.method:
                 raise self.reject( 'Bad authorization data method does not match')
-            self.logger.debug('Path raw(%s) signed(%s) refined(%s) source(%s)',
+            if content['path'] != path:
+                self.logger.debug('Path raw(%s) signed(%s) refined(%s) source(%s)',
                                   bottle.request.path, content['path'],
                                   path, bottle.request.environ['PATH_INFO'])
-            if content['path'] != path:
                 raise self.reject( 'Bad authorization data path does not match')
         except KeyError, e:
             raise self.reject( 'Missing authentication data: %s' %e)
