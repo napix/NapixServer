fddc7c56be1f8ab2094b6244cde3c4f94e021dec
Grégoire ROCHER <gr@enix.org>
2013-09-25 15:44:39 +0200

    Add documentation on the plugin

--- napixd/plugins/exceptions.py
+++ napixd/plugins/exceptions.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+"""
+:mod:`bottle` plugin for the exceptions
+"""
+
 import sys
 import os.path
 import traceback
@@ -15,6 +19,15 @@ import napixd
 
 
 class ExceptionsCatcher(object):
+    """
+    Catch the exception raised by the decorated function.
+
+    When an exception is caught, the traceback, the exception value
+    and the details of the error are extracted and returned in a :class:`dict`.
+
+    If *pprint* is True, the produced JSON will be indented.
+    If *show_errors* is True, the exceptions are printed on the console.
+    """
     name = 'exceptions_catcher'
     api = 2
     logger = logging.getLogger('Napix.Errors')
@@ -25,6 +38,12 @@ class ExceptionsCatcher(object):
         self.pprint = 4 if pprint else None
 
     def extract_error(self, error):
+        """
+        Extract a :class:`dict` from an exception.
+
+        It adds the keys ``request`` containing the details of the HTTP request
+        causing the exception.
+        """
         a, b, last_traceback = sys.exc_info()
         method = bottle.request.method
         path = bottle.request.path
@@ -44,12 +63,25 @@ class ExceptionsCatcher(object):
         return res
 
     def remote_exception(self, error):
+        """
+        When the error is a :exc:`napix.exceptions.HTTPError`,
+        it adds the keys ``remote_call`` with the remote request description
+        and ``remote_error`` with the detail of the error.
+        """
         return {
             'remote_call': unicode(error.request),
             'remote_error': error.remote_error or str(error)
         }
 
     def traceback_info(self, last_traceback):
+        """
+        Extracts the informations from the traceback.
+
+        Add the keys ``filename`` and ``line`` pointing to the root
+        of the exception.
+        Also adds the key ``traceback`` containing a dump of the traceback
+        as a :class:`list`.
+        """
         all_tb = [dict(zip(('filename', 'line', 'in', 'call'), x))
                   for x in traceback.extract_tb(last_traceback)]
         extern_tb = [x for x in all_tb
@@ -63,6 +95,10 @@ class ExceptionsCatcher(object):
         }
 
     def exception_details(self, error):
+        """
+        Adds the key ``error_text`` with the string value of the exception and
+        the key ``error_class`` with the class name of the exception.
+        """
         return {
             'error_text': str(error),
             'error_class': error.__class__.__name__,
