0acf03d8075b0c7af0f981a43f438196cb430841
Grégoire ROCHER <gr@enix.org>
2013-11-04 16:21:45 +0100

    add a laoded property in the resource wrapper

--- napixd/services/wrapper.py
+++ napixd/services/wrapper.py
@@ -41,6 +41,10 @@ class ResourceWrapper(collections.Mapping):
         if resource:
             self.resource = resource
 
+    @property
+    def loaded(self):
+        return 'resource' in self.__dict__
+
     def __len__(self):
         return len(self.resource)
 
@@ -63,7 +67,7 @@ class ResourceWrapper(collections.Mapping):
     def __repr__(self):
         return 'Resource {0} of `{1}` {2}'.format(
             self.id, self.manager.get_name(),
-            'loaded' if 'resource' in self.__dict__ else 'not loaded')
+            'loaded' if self.loaded else 'not loaded')
 
     def __eq__(self, other):
         return (
