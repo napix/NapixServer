08f6eb6c660710bb4a6fb20a0ba56cdf612f8810
Grégoire ROCHER <gr@enix.org>
2013-11-29 15:05:03 +0100

    Remove bottle reference

--- napixd/plugins/ratelimit.py
+++ napixd/plugins/ratelimit.py
@@ -5,11 +5,8 @@
 Limit the requests launched by users.
 """
 
-import functools
-
-import bottle
-
 from napixd.store import Counter
+from napixd.http.response import HTTPError
 
 
 class RateLimitingPlugin(object):
@@ -22,38 +19,28 @@ class RateLimitingPlugin(object):
     Requests denied because of the rate limit will be responded
     by a 429 status code.
     """
-    name = 'rate_limit'
-    api = 2
 
     def __init__(self, conf):
         self.conf = conf
 
-    def apply(self, callback, route):
-        @functools.wraps(callback)
-        def inner_rate_limit(*args, **kwargs):
-            if not self.is_under_limit():
-                raise bottle.HTTPError(429, 'You exceeded your quota')
-            return callback(*args, **kwargs)
-        return inner_rate_limit
+    def __call__(self, callback, request):
+        if not self.is_under_limit():
+            raise HTTPError(429, 'You exceeded your quota')
+        return callback(request)
 
     def is_under_limit(self):
         return True
 
 
 class ConcurrentLimiter(object):
-    name = 'concurrent_limit'
-    api = 2
-
     def __init__(self, max=2):
         self.max = max
 
-    def apply(self, callback, route):
-        @functools.wraps(callback)
-        def inner_concurrent_limiter(*args, **kw):
-            with self.get_counter() as concurrent:
-                if concurrent > self.max:
-                    raise bottle.HTTPError(429, 'Too many concurrent connections')
-                return callback(*args, **kw)
+    def __call__(self, callback, request):
+        with self.get_counter(request) as concurrent:
+            if concurrent > self.max:
+                raise HTTPError(429, 'Too many concurrent connections')
+        return callback(request)
 
-    def get_counter(self):
-        return Counter(bottle.request.environ['REMOTE_ADDR'])
+    def get_counter(self, request):
+        return Counter(request.environ['REMOTE_ADDR'])
