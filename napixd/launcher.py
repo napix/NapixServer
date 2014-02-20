1dd7114af5da49fcbb41b15ff13357f87acf0413
Grégoire ROCHER <gr@enix.org>
2014-02-20 15:50:44 +0100

    Add specific retrun codes

--- napixd/launcher.py
+++ napixd/launcher.py
@@ -83,10 +83,12 @@ def launch(options, setup_class=None):
         setup = setup_class(options, port=keys.port)
     except CannotLaunch as e:
         logger.critical(e)
+        sys.exit(1)
         return
     except Exception as e:
         logger.exception(e)
         logger.critical(e)
+        sys.exit(-1)
         return
 
     try:
@@ -97,6 +99,7 @@ def launch(options, setup_class=None):
         if 'print_exc' in setup.options:
             logger.exception(e)
         logger.critical(e)
+        sys.exit(3)
 
 
 class CannotLaunch(Exception):
