43885b3fbb1fa2789d7ba51994c89761fa35b7df
Grégoire ROCHER <gr@enix.org>
2014-02-21 15:46:43 +0100

    add a constructor by direction

--- napixd/managers/validators/date.py
+++ napixd/managers/validators/date.py
@@ -22,9 +22,29 @@ def ISO8601_datetime(iso_string):
 
 
 class DatetimeBoundary(object):
-    def __init__(self, allow_past=True, allow_future=True):
+    def __init__(self, direction=None, allow_past=True, allow_future=True):
+        if direction is not None:
+            if direction not in ('past', 'future'):
+                raise ValueError('Direction must be past or future')
+            allow_future = direction == 'future'
+            allow_past = direction == 'past'
+
         self.allow_future = allow_future
         self.allow_past = allow_past
+
+    def __repr__(self):
+        if self.allow_past:
+            return '<Datetime in the past>'
+        else:
+            return '<Datetime in the future>'
+
+    def __eq__(self, other):
+        return (isinstance(other, DatetimeBoundary) and
+                self.allow_past == other.allow_past and
+                self.allow_future == other.allow_future)
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
 
     def __call__(self, date):
         now = datetime.datetime.now()
