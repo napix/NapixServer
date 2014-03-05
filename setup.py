a3359122315157ef587281325def3e1e8564bd54
Grégoire ROCHER <gr@enix.org>
2014-03-05 11:59:57 +0100

    Bump the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="napixd",
-    version="1.7.2",
+    version="1.7.3",
     packages=find_packages(
         exclude=[
             'napixd.examples',
