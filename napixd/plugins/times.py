be9e9da7b7f5f2cfadb852a8476764228d2f890d
Grégoire ROCHER <gr@enix.org>
2014-04-25 11:55:38 +0200

    Add a Plugin to wait for a givent amout of time

--- napixd/plugins/times.py
+++ napixd/plugins/times.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import time
+
 from napixd.chrono import Chrono
 from napixd.http.response import HTTPResponse
 
@@ -19,3 +21,18 @@ class TimePlugin(object):
             resp = callback(request)
 
         return HTTPResponse({self.header_name: chrono.total}, resp)
+
+
+class WaitPlugin(object):
+    def __init__(self, wait_time):
+        self._wait = wait_time / 1000.
+
+    def __call__(self, callback, request):
+        with Chrono() as chrono:
+            resp = callback(request)
+
+        wait = self._wait - chrono.total
+
+        if wait > 0:
+            time.sleep(wait)
+        return resp
