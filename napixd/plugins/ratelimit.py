067f8d28aff96c9c20a74406e382c95b0eb7af37
Grégoire ROCHER <gr@enix.org>
2014-05-05 12:46:00 +0200

    Add ratelimiter

--- napixd/plugins/ratelimit.py
+++ napixd/plugins/ratelimit.py
@@ -5,31 +5,63 @@
 Limit the requests launched by users.
 """
 
+import time
+import logging
+
 from napixd.store import Counter
+from napixd.utils.connection import ConnectionFactory
 from napixd.http.response import HTTPError
+from napixd.conf.lazy import LazyConf
+
+logger = logging.getLogger('Napix.ratelimit')
+
+
+connection_factory = ConnectionFactory(LazyConf('redis'))
 
 
 class RateLimitingPlugin(object):
     """
-    Limit the number of requests made by users.
+    Limits the number of requests made by users to *max* during *timespan*.
 
-    The *conf* parameter is a :class:`napixd.conf.Conf` instance
-    with the parameters of the rate limit.
-
-    Requests denied because of the rate limit will be responded
-    by a 429 status code.
+    When a request is denied, a 429 status response is returned.
     """
 
-    def __init__(self, conf):
-        self.conf = conf
+    @classmethod
+    def from_settings(cls, settings):
+        max = settings.get('max', type=int)
+        timespan = settings.get('timespan', type=int)
+        con = connection_factory(settings.get('connection'))
+        logger.info('Ratelimiting to %s/%ss via %s', max, timespan, con)
+        return cls(max, timespan, con)
+
+    def __init__(self, max, timespan, con):
+        self._max = max
+        self._timespan = timespan
+        self._con = con
 
     def __call__(self, callback, request):
-        if not self.is_under_limit():
-            raise HTTPError(429, 'You exceeded your quota')
+        criteria = self.get_criteria(request)
+        if not self.is_under_limit(criteria):
+            logger.warning('Rejecting request of %s, quota maxed', criteria)
+            return HTTPError(429, 'You exceeded your quota')
+
         return callback(request)
 
-    def is_under_limit(self):
-        return True
+    def get_criteria(self, request):
+        return request.environ.get('REMOTE_ADDR', '?')
+
+    def is_under_limit(self, criteria):
+        key = 'rate_limit:{0}'.format(criteria)
+        period_end = time.time()
+        period_start = period_end - self._timespan
+
+        with self._con.pipeline() as pipe:
+            pipe.zcount(key, period_start, period_end)
+            pipe.zadd(key, period_end, period_end)
+            pipe.expireat(key, int(period_end + self._timespan))
+            count, add, xat = pipe.execute()
+
+        return count < self._max
 
 
 class ConcurrentLimiter(object):
--- tests/test_plugins/test_ratelimit.py
+++ tests/test_plugins/test_ratelimit.py
@@ -8,11 +8,43 @@ import unittest
 import mock
 
 from napixd.plugins.ratelimit import RateLimitingPlugin
-from napixd.conf import Conf
 
 
 class TestRateLimit(unittest.TestCase):
     def setUp(self):
-        self.success = mock.MagicMock(__name__='callback')
-        self.rl = RateLimitingPlugin(Conf())
-        self.cb = self.rl.apply(self.success, mock.Mock())
+        #2 requests max by 60 seconds
+        self.con = mock.MagicMock()
+        self.pipe = self.con.pipeline.return_value.__enter__.return_value = mock.Mock()
+        self.rl = RateLimitingPlugin(2, 60, self.con)
+
+    def call(self):
+        self.cb = mock.Mock(name='callback')
+        self.req = mock.Mock(
+            name='request',
+            environ={
+                'REMOTE_ADDR': '191.144.12.13',
+            }
+        )
+        with mock.patch('napixd.plugins.ratelimit.time') as time:
+            time.time.return_value = 1200
+            return self.rl(self.cb, self.req)
+
+    def test_first_req(self):
+        self.pipe.execute.return_value = [0, True, 0]
+        resp = self.call()
+        self.pipe.assert_has_calls([
+            mock.call.zcount('rate_limit:191.144.12.13', 1140, 1200),
+            mock.call.zadd('rate_limit:191.144.12.13', 1200, 1200),
+            mock.call.expireat('rate_limit:191.144.12.13', 1260),
+        ])
+        self.assertEqual(resp, self.cb.return_value)
+
+    def test_second_request(self):
+        self.pipe.execute.return_value = [1, True, 0]
+        resp = self.call()
+        self.assertEqual(resp, self.cb.return_value)
+
+    def test_third_request(self):
+        self.pipe.execute.return_value = [2, True, 0]
+        resp = self.call()
+        self.assertEqual(resp.status, 429)
